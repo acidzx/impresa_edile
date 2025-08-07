e34de4e3d836c979f3581ff3c424694637474a71
acidzx <acidxtech@gmail.com>
2025-08-07 17:34:18 +0200

    Update menu.tsx

--- components/menu.tsx
+++ components/menu.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { MenuIcon, XIcon } from "lucide-react";
+import Link from "next/link";
 
 const navigation = [
   { name: "Home", href: "/" },
@@ -18,7 +19,7 @@ export default function Menu() {
         aria-label="Global"
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
       >
-        <a href="/" className="-m-1.5 p-1.5">
+        <Link href="/" className="-m-1.5 p-1.5">
           <h1 className="text-2xl font-black underline decoration-2 underline-offset-4 decoration-[rgb(118,255,0)] tracking-tighter">
             STED <span className="font-semibold">srl</span>
           </h1>
@@ -28,7 +29,7 @@ export default function Menu() {
             src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=lime&shade=400"
             className="h-8 w-auto"
           /> */}
-        </a>
+        </Link>
         <div className="flex lg:hidden">
           <button
             type="button"
@@ -41,13 +42,13 @@ export default function Menu() {
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a
+            <Link
               key={item.name}
               href={item.href}
               className="text-sm/6 font-semibold text-gray-900 hover:text-lime-500"
             >
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
       </nav>
@@ -59,7 +60,7 @@ export default function Menu() {
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link href="#" className="-m-1.5 p-1.5">
               <h1 className="text-2xl font-black underline decoration-2 underline-offset-4 decoration-[rgb(118,255,0)] tracking-tighter">
                 STED <span className="font-semibold">srl</span>
               </h1>
@@ -69,7 +70,7 @@ export default function Menu() {
                 src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=lime&shade=400"
                 className="h-8 w-auto"
               /> */}
-            </a>
+            </Link>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
@@ -83,13 +84,13 @@ export default function Menu() {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
             </div>
