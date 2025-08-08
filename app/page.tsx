8befe20755cd0b37d08982f1cadac47cb32dbb7f
acidzx <acidxtech@gmail.com>
2025-08-08 13:39:50 +0200

    up

--- app/page.tsx
+++ app/page.tsx
@@ -111,7 +111,7 @@ export default function Home() {
         </div>
         <div className="w-full lg:w-1/2">
           <Image
-            src="https://grassiecrespi.it/images/_MG_1205.png"
+            src="/img2.jpg"
             alt="logo"
             width={1000}
             height={1000}
@@ -159,7 +159,7 @@ export default function Home() {
         </div>
         <div className="w-full lg:w-1/2">
           <Image
-            src="https://grassiecrespi.it/images/_MG_1205.png"
+            src="/img3.jpg"
             alt="logo"
             width={1000}
             height={1000}
